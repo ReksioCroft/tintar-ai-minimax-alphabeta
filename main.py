5119d5384cb961368a2284e6f092cc379a898a1e
Octavian-Florin  Staicu <octavian-florin.staicu@my.fmi.unibuc.ro>
2021-05-31 18:10:25 +0300

    Generare Succesori

--- main.py
+++ main.py
@@ -1,26 +1,158 @@
 import tkinter
 import functools
+import copy
 
 
 class Stare:
-    def __init__(self, piese_albe_nefolosite=9, piese_albe_pe_tabla=0, piese_negre_nefolosite=9, piese_negre_pe_tabla=0, jucator_curent="alb", tata=None, matrix=None):
+    decodif_poz_matrice = [(1, 1), (1, 4), (1, 7), (2, 2), (2, 4), (2, 6), (3, 3), (3, 4), (3, 5), (4, 1), (4, 2), (4, 3), (4, 5), (4, 6), (4, 7), (5, 3), (5, 4), (5, 5), (6, 2), (6, 4), (6, 6), (7, 1), (7, 4), (7, 7)]
+
+    def __init__(self, piese_albe_nefolosite=9, piese_albe_pe_tabla=0, piese_negre_nefolosite=9, piese_negre_pe_tabla=0, jucator_curent_alb=True, tata=None, matrix=None, se_scoate_o_piesa=False):
         """
         Starea este retinuta sub forma unei matrici 8x3, cate o linie pt fiecare rand (linia din mijloc cu 6 intrari se considera doua linii separate, una pt stanga si una pt dreapta)
         :param piese_albe_nefolosite: nr de piese ce mai trb puse pe tabla (etapa1). Initial fiecare jucator are 9 piese
         :param piese_albe_pe_tabla: cate piese se gasesc pe tabla (fara cele din mana si fara cele scoase din joc)
         :param piese_negre_nefolosite: idem
         :param piese_negre_pe_tabla: idem
-        :param jucator_curent: ce jucator urmeaza sa mute. Intotdeauna incepe jucatorul alb
+        :param jucator_curent_alb: ce jucator urmeaza sa mute. True daca e alb, False daca e Negru
         :param tata: starea din care s-a generat aceasta mutare
         :param matrix: matricea actuala a tablei de bord
         """
         self.tata = tata
-        self.matrix = matrix if matrix is not None else [[0] * 3 for _ in range(8)]
+        self.matrix = matrix if matrix is not None else [[None for _ in range(3)] for _ in range(8)]
         self.piese_albe_nefolosite = piese_albe_nefolosite
         self.piese_albe_pe_tabla = piese_albe_pe_tabla
         self.piese_negre_nefolosite = piese_negre_nefolosite
         self.piese_negre_pe_tabla = piese_negre_pe_tabla
-        self.jucator_curent = jucator_curent
+        self.jucator_curent_alb = jucator_curent_alb
+        self.se_scoate_o_piesa = se_scoate_o_piesa
+        self.l_succesori = None
+
+    def __eq__(self, o):
+        if self.__class__ == o.__class__:
+            return self.matrix == o.matrix and self.jucator_curent_alb == o.jujucator_curent_alb and self.piese_albe_nefolosite == o.piese_albe_nefolosite and \
+                   self.piese_albe_pe_tabla == o.piese_albe_pe_tabla and self.piese_negre_nefolosite == o.piese_negre_nefolosite and self.piese_negre_pe_tabla == o.piese_negre_pe_tabla
+        else:
+            return False
+
+    @classmethod
+    def este_in_moara(cls, matrix, poz):
+        lin_reala, col_reala = cls.decodif_poz_matrice[poz[0] * 3 + poz[1]]
+        okMoaraCol = True
+        okMoaraLin = True
+        for i in range(len(cls.decodif_poz_matrice)):
+            lin_in_matrice = i // 3
+            col_in_matrice = i % 3
+            if cls.decodif_poz_matrice[i][0] == lin_reala:
+                if matrix[lin_in_matrice][col_in_matrice] != matrix[poz[0]][poz[1]]:
+                    okMoaraCol = False
+            if cls.decodif_poz_matrice[i][1] == col_reala:
+                if matrix[lin_in_matrice][col_in_matrice] != matrix[poz[0]][poz[1]]:
+                    okMoaraLin = False
+        return okMoaraLin or okMoaraCol
+
+    def generare_succesori(self):
+        """
+        Va parcurge toate elementele din matrix. Daca nu mai sunt piese de adaugat, pentru elementele care au aceeasi culoare cu a jucatorului curent,
+        se vor incerca din cele maxim 4 directi posibile de deplasare, care sunt libere si se va genera noul succesor
+        :return: intoarce lista mutarilor posibile pentru jucatorul curent
+        """
+        if self.l_succesori is None:
+            self.l_succesori = []
+            for i in range(8):
+                for j in range(3):
+                    if self.se_scoate_o_piesa:
+                        if self.matrix[i][j] is not None and not self.jucator_curent_alb == self.matrix[i][j] and not Stare.este_in_moara(self.matrix, (i, j)):
+                            new_matrix = copy.deepcopy(self.matrix)
+                            new_matrix[i][j] = None
+                            negre_ramase = self.piese_negre_pe_tabla
+                            albe_ramase = self.piese_albe_pe_tabla
+                            if self.jucator_curent_alb:
+                                negre_ramase -= 1
+                            else:
+                                albe_ramase -= 1
+                            stare_noua = Stare(tata=self, matrix=new_matrix, piese_albe_nefolosite=self.piese_albe_nefolosite, piese_albe_pe_tabla=albe_ramase,
+                                               piese_negre_pe_tabla=negre_ramase, piese_negre_nefolosite=self.piese_negre_nefolosite, se_scoate_o_piesa=False, jucator_curent_alb=not self.jucator_curent_alb)
+                            self.l_succesori.append(stare_noua)
+                    else:
+                        if (self.jucator_curent_alb and self.piese_albe_nefolosite == 0) or (not self.jucator_curent_alb and self.piese_negre_nefolosite == 0):
+                            if (self.jucator_curent_alb and self.piese_albe_pe_tabla > 3) or (not self.jucator_curent_alb and self.piese_negre_pe_tabla > 3):
+                                for lin, col in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                                    try:
+                                        """
+                                        obtinem linia si coloana reala a pozitiei curente. Apoi ne uitam sus, jos, stanga, dreapta, iar pt acele pozitii care exista
+                                        obtinem linia si coloana din matrice la care se afla elementul adiacent. Daca este o pozitie libera, generam un nou succesor
+                                        """
+                                        linie_reala_actuala, coloana_reala_actuala = Stare.decodif_poz_matrice[i * 3 + j]
+                                        idx_pozitie_noua = Stare.decodif_poz_matrice.index((linie_reala_actuala + lin, coloana_reala_actuala + col))
+                                        lin_pozitie_noua = idx_pozitie_noua // 3
+                                        col_pozitie_noua = idx_pozitie_noua % 3
+                                        if self.matrix[lin_pozitie_noua][col_pozitie_noua] is None:
+                                            new_matrix = copy.deepcopy(self.matrix)
+                                            new_matrix[i][j] = None
+                                            new_matrix[lin_pozitie_noua][col_pozitie_noua] = self.jucator_curent_alb
+                                            eliminare = Stare.este_in_moara(new_matrix, (lin_pozitie_noua, col_pozitie_noua))
+                                            if eliminare:
+                                                jucator_nou = self.jucator_curent_alb
+                                            else:
+                                                jucator_nou = not self.jucator_curent_alb
+                                            stare_noua = Stare(tata=self, matrix=new_matrix, piese_albe_nefolosite=self.piese_albe_nefolosite, piese_albe_pe_tabla=self.piese_albe_pe_tabla,
+                                                               piese_negre_pe_tabla=self.piese_negre_pe_tabla, piese_negre_nefolosite=self.piese_negre_nefolosite, se_scoate_o_piesa=eliminare, jucator_curent_alb=jucator_nou)
+                                            self.l_succesori.append(stare_noua)
+                                    except Exception:
+                                        continue
+                            else:
+                                """
+                                au ramas 3 piese pentru jucatorul curent, deci poate sa sara cum vrea
+                                """
+                                if self.matrix[i][j] == self.jucator_curent_alb:
+                                    for l in range(8):
+                                        for c in range(3):
+                                            if self.matrix[l][c] is None:
+                                                new_matrix = copy.deepcopy(self.matrix)
+                                                new_matrix[i][j] = None
+                                                new_matrix[l][c] = self.jucator_curent_alb
+                                                eliminare = Stare.este_in_moara(new_matrix, (l, c))
+                                                if eliminare:
+                                                    jucator_nou = self.jucator_curent_alb
+                                                else:
+                                                    jucator_nou = not self.jucator_curent_alb
+                                                stare_noua = Stare(tata=self, matrix=new_matrix, piese_albe_nefolosite=self.piese_albe_nefolosite, piese_albe_pe_tabla=self.piese_albe_pe_tabla,
+                                                                   piese_negre_pe_tabla=self.piese_negre_pe_tabla, piese_negre_nefolosite=self.piese_negre_nefolosite, se_scoate_o_piesa=eliminare, jucator_curent_alb=jucator_nou)
+                                                self.l_succesori.append(stare_noua)
+
+                        else:
+                            """
+                            adaugam o noua piesa
+                            """
+                            if self.matrix[i][j] is None:
+                                negre_nefolosite = self.piese_negre_nefolosite
+                                negre_ramase = self.piese_negre_pe_tabla
+                                albe_nefolosite = self.piese_albe_nefolosite
+                                albe_ramase = self.piese_albe_pe_tabla
+                                if self.jucator_curent_alb:
+                                    albe_ramase += 1
+                                    albe_nefolosite -= 1
+                                else:
+                                    negre_ramase += 1
+                                    negre_nefolosite -= 1
+                                new_matrix = copy.deepcopy(self.matrix)
+                                new_matrix[i][j] = self.jucator_curent_alb
+                                eliminare = Stare.este_in_moara(new_matrix, (i, j))
+                                if eliminare:
+                                    jucator_nou = self.jucator_curent_alb
+                                else:
+                                    jucator_nou = not self.jucator_curent_alb
+                                stare_noua = Stare(tata=self, matrix=new_matrix, piese_albe_nefolosite=albe_nefolosite, piese_albe_pe_tabla=albe_ramase,
+                                                   piese_negre_pe_tabla=negre_ramase, piese_negre_nefolosite=negre_nefolosite, se_scoate_o_piesa=eliminare, jucator_curent_alb=jucator_nou)
+                                self.l_succesori.append(stare_noua)
+
+        return self.l_succesori
+
+
+def f(s: Stare):
+    l = s.generare_succesori()
+    for i in l:
+        print(i.matrix)
 
 
 class MorrisBoard(tkinter.Tk):
@@ -34,119 +166,19 @@ class MorrisBoard(tkinter.Tk):
         bg = tkinter.PhotoImage(file="board13.png")
         label = tkinter.Label(self.board_frame, image=bg)
         label.place(x=0, y=0)
-        self.add_buttons(5)
         self.stare_initiala = Stare()
 
-        # self.board_frame.grid_propagate(True)
+        self.add_buttons(5)
 
-        self.board_frame.grid(row=1, column=1)  # put frame where the button should be
+        self.board_frame.grid(row=1, column=1)
 
         self.mainloop()
 
     def add_buttons(self, dim):
-        # Linia 1 (cea mai sus)
-        button1 = tkinter.Button(self.board_frame, height=dim, width=dim, )  # command=functools.partial(f, 3))
-        button1.grid(row=1, column=1)
-        MorrisBoard.buttons.append((button1, (1, 1)))
-
-        button2 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button2.grid(row=1, column=4)
-        MorrisBoard.buttons.append((button2, (1, 4)))
-
-        button3 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button3.grid(row=1, column=7)
-        MorrisBoard.buttons.append((button3, (1, 7)))
-
-        # linia 2 (mijloc sus)
-        button4 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button4.grid(row=2, column=2)
-        MorrisBoard.buttons.append((button4, (2, 2)))
-
-        button5 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button5.grid(row=2, column=4)
-        MorrisBoard.buttons.append((button5, (2, 4)))
-
-        button6 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button6.grid(row=2, column=6)
-        MorrisBoard.buttons.append((button6, (2, 6)))
-
-        # linia 3(inf sus)
-        button7 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button7.grid(row=3, column=3)
-        MorrisBoard.buttons.append((button7, (3, 3)))
-
-        button8 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button8.grid(row=3, column=4)
-        MorrisBoard.buttons.append((button8, (3, 4)))
-
-        button9 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button9.grid(row=3, column=5)
-        MorrisBoard.buttons.append((button9, (3, 5)))
-
-        # linia 4 (Mijloc stanga)
-        button10 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button10.grid(row=4, column=1)
-        MorrisBoard.buttons.append((button10, (4, 1)))
-
-        button11 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button11.grid(row=4, column=2)
-        MorrisBoard.buttons.append((button11, (4, 2)))
-
-        button12 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button12.grid(row=4, column=3)
-        MorrisBoard.buttons.append((button12, (4, 3)))
-
-        # linia 5 (Mijloc dreapta)
-        button13 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button13.grid(row=4, column=5)
-        MorrisBoard.buttons.append((button13, (4, 5)))
-
-        button14 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button14.grid(row=4, column=6)
-        MorrisBoard.buttons.append((button14, (4, 6)))
-
-        button15 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button15.grid(row=4, column=7)
-        MorrisBoard.buttons.append((button15, (4, 7)))
-
-        # linia 6 (sup jos)
-        button16 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button16.grid(row=5, column=3)
-        MorrisBoard.buttons.append((button16, (5, 3)))
-
-        button17 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button17.grid(row=5, column=4)
-        MorrisBoard.buttons.append((button17, (5, 4)))
-
-        button18 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button18.grid(row=5, column=5)
-        MorrisBoard.buttons.append((button18, (5, 5)))
-
-        # linia 7 (mij jos)
-        button19 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button19.grid(row=6, column=2)
-        MorrisBoard.buttons.append((button19, (6, 2)))
-
-        button20 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button20.grid(row=6, column=4)
-        MorrisBoard.buttons.append((button20, (6, 4)))
-
-        button21 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button21.grid(row=6, column=6)
-        MorrisBoard.buttons.append((button21, (6, 6)))
-
-        # linia 8 (inf jos)
-        button22 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button22.grid(row=7, column=1)
-        MorrisBoard.buttons.append((button22, (7, 1)))
-
-        button23 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button23.grid(row=7, column=4)
-        MorrisBoard.buttons.append((button23, (7, 4)))
-
-        button24 = tkinter.Button(self.board_frame, height=dim, width=dim)
-        button24.grid(row=7, column=7)
-        MorrisBoard.buttons.append((button24, (7, 7)))
+        for lin, col in Stare.decodif_poz_matrice:
+            button = tkinter.Button(self.board_frame, height=dim, width=dim, command=functools.partial(f, self.stare_initiala))
+            button.grid(row=lin, column=col)
+            MorrisBoard.buttons.append(button)
 
 
 if __name__ == "__main__":
